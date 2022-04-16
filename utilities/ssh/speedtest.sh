48bc7d5ab4e3383896474c051345c3e402993a03
Henry Walter <airstrikeminecraft@gmail.com>
2022-04-16 22:18:24 +0100

    Update speedtest.sh

--- utilities/ssh/speedtest.sh
+++ utilities/ssh/speedtest.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
+clear
 echo '• Downloading speedtest-cli'
 sudo curl -s -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py 
 sudo chmod +x speedtest-cli
 echo '• Downloaded speedtest-cli'
-echo '• Beginning test'
+clear
+echo '• Beginning test\n\n'
 
 sudo python3 speedtest-cli --share
 
-echo '• Test finished, Deleting speedtest-cli'
+echo '\n\n• Test finished, Deleting speedtest-cli'
 sudo rm speedtest-cli
