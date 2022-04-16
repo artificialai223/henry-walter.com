47956f8a410d2b3677889e5949ab645b3e79e084
Henry Walter <airstrikeminecraft@gmail.com>
2022-04-16 22:06:09 +0100

    Create speedtest.sh

---
+++ utilities/ssh/speedtest.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+echo '• Downloading speedtest-cli'
+sudo curl -s -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py 
+sudo chmod +x speedtest-cli
+echo '• Downloaded speedtest-cli'
+echo '• Beginning test'
+
+sudo speedtest-cli --share
+
+echo '• Test finished, Deleting speedtest-cli'
+sudo rm speedtest-cli
